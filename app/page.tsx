cb64cf409a5003925adad5be96ea0b8232c9cfdb
Graham Van Pelt <grahamvanpelt@gmail.com>
2023-04-20 13:23:06 -0700

    tabler

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,23 @@
 import Image from "next/image"
 import heroImage from "/public/This_Is_How_I_Know_You-4-scaled.jpg"
 import Link from "next/link"
-import { CiInstagram, CiMail } from "react-icons/ci"
+import { TbMail } from "react-icons/tb"
 
 export default async function Home() {
   return (
     <>
       <section className="flex flex-col gap-4 mb-8">
         <h1 className="text-xl">Art / Design / Commercial Photography</h1>
-        <h2 className="text-l">Based on Salt Spring Island, BC</h2>
-        <div className="flex gap-2">
-          <CiMail size={20} />
-          <Link href="mailto:sophialibertyburke@gmail.com">Email Me</Link>
+        <h2 className="text-base">Based on Salt Spring Island, BC</h2>
+        <div>
+          <Link
+            href="mailto:sophialibertyburke@gmail.com"
+            className="flex text-base gap-2"
+          >
+            {" "}
+            <TbMail size={21} className="" />
+            <span>Email</span>
+          </Link>
         </div>
         {/* <div className="flex gap-2">
           <CiInstagram size={20} />
