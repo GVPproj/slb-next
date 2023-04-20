071e06f02aecc2f4b031c1f5241931ea11d9abd2
Graham Van Pelt <grahamvanpelt@gmail.com>
2023-04-20 13:12:38 -0700

    uppercase, react icons

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image"
 import heroImage from "/public/This_Is_How_I_Know_You-4-scaled.jpg"
 import Link from "next/link"
+import { CiInstagram, CiMail } from "react-icons/ci"
 
 export default async function Home() {
   return (
@@ -8,22 +9,18 @@ export default async function Home() {
       <section className="flex flex-col gap-4 mb-8">
         <h1 className="text-xl">Art / Design / Commercial Photography</h1>
         <h2 className="text-l">Based on Salt Spring Island, BC</h2>
-        <div className="flex gap-4 ">
-          <Link href="mailto:sophialibertyburke@gmail.com">
-            <button className="contact-btn" type="button">
-              Email
-            </button>
-          </Link>
-          <Link href="https://www.instagram.com/burke_sophia/">
-            <button className="contact-btn" type="button">
-              Instagram
-            </button>
-          </Link>
+        <div className="flex gap-2">
+          <CiMail size={20} />
+          <Link href="mailto:sophialibertyburke@gmail.com">Email Me</Link>
         </div>
+        {/* <div className="flex gap-2">
+          <CiInstagram size={20} />
+          <Link href="https://instagram.com/burke_sophia">Instagram</Link>
+        </div> */}
       </section>
       <Image
         width="1300"
-        className=""
+        className="mb-8"
         src={heroImage}
         alt="Image of gallery installation piece"
       />
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,7 +7,7 @@ export default function Navbar() {
   // const [show, setShow] = useState(false)
 
   return (
-    <header className="fixed top-0 z-50 w-screen backdrop-blur-md bg-stone-100/70 mb-16">
+    <header className="fixed top-0 z-50 w-screen backdrop-blur-md bg-stone-100/70 mb-16 uppercase">
       <nav className=" max-w-5xl mx-auto w-11/12 my-3 flex justify-between">
         <Link
           className="text-3xl lg:-ml-2 outline-none outline-dashed focus:outline-slate-200 "
