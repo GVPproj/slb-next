07f607b8a380631bbb7cf22130cc08ef093d328a
Graham Van Pelt <grahamvanpelt@gmail.com>
2023-04-20 09:15:01 -0700

    flex-col on small screen footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ import Link from "next/link"
 export default function Footer() {
   return (
     <footer>
-      <div className="flex justify-between w-11/12 max-w-5xl mx-auto mt-auto mb-7 ">
+      <div className="flex flex-col sm:flex-row justify-between w-11/12 max-w-5xl mx-auto mt-auto mb-7 ">
         <p>images &copy; sophia burke, {new Date().getFullYear()}</p>
         <p>
           website:{" "}
