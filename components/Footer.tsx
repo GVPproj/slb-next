7fac20abf0cb1ab88aa527d3da5b262630a00fc8
Graham Van Pelt <grahamvanpelt@gmail.com>
2023-05-20 05:58:58 -0700

    capitalized titles

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,9 +4,9 @@ export default function Footer() {
   return (
     <footer>
       <div className="text-stone-900/70 flex flex-col sm:flex-row justify-between w-11/12 max-w-5xl mx-auto mt-auto mb-7 pt-8">
-        <p>images &copy; sophia burke, {new Date().getFullYear()}</p>
+        <p>Images &copy; Sophia Burke, {new Date().getFullYear()}</p>
         <p>
-          website:{" "}
+          Website:{" "}
           <Link href="https://grahamvanpelt.dev">grahamvanpelt.dev</Link>
         </p>
       </div>
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,21 +7,21 @@ export default function Navbar() {
   // const [show, setShow] = useState(false)
 
   return (
-    <header className="fixed top-0 z-50 w-screen backdrop-blur-md bg-stone-100/70 mb-16 uppercase">
+    <header className="fixed top-0 z-50 w-screen backdrop-blur-md bg-stone-100/70 mb-16">
       <nav className=" max-w-5xl mx-auto w-11/12 my-3 flex justify-between">
         <Link
           className="text-3xl lg:-ml-2 outline-none outline-dashed focus:outline-slate-200 "
           href="/"
         >
-          sophia burke
+          Sophia Burke
         </Link>
 
         <div className="hidden md:flex flex-row gap-6 items-end">
           <Link className="text-lg" href="/archives">
-            archives
+            Archives
           </Link>
           <Link className="text-lg" href="/about">
-            about
+            About
           </Link>
         </div>
         <div className="md:hidden">
